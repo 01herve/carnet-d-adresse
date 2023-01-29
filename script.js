c48898fd3338fc44b27c5329dac729d7902f0b2a
herve <hervengalamulume1@gmail.com>
2023-01-29 10:26:19 +0100

    232

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const form = document.querySelector('form')
 const tab = []
+let isModifying = false;
+let modifyIndex = -1
 
 form.addEventListener('submit', event => {
     event.preventDefault()
     const obj_contact = Object.fromEntries(new FormData(form))
+    if(!isModifying){
     tab.push(obj_contact)
-    render()
+    }else{tab[modifyIndex] = obj_contact;modifyIndex = -1;isModifying = false}
+     render(tab)
 })
 
 function render() {
